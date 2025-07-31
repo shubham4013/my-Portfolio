041dc2c606a70cddbd11d8b1fe4af8a754028cab
Shubham kumar <93448241+shubham4013@users.noreply.github.com>
2025-07-31 16:50:52 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Java Language", "C++ Language", "Data Structure", "web development"],
+    strings: ["Java Language", "C++ Language", "SQL ", "web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
